1078d7da5cb937f9275ec8f6f74fdf9666744481
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2019-08-13 12:27:43 +0300

    Implement hierarchy search

--- ckanext/hierarchy_search/plugin.py
+++ ckanext/hierarchy_search/plugin.py
@@ -1,9 +1,45 @@
+import re
+
+import ckan.model as model
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+
+from ckan.common import g
+
+RE_ORG_FQ = re.compile(r'owner_org:(\'|")?([^\s\'"]+)(?(1)\1)')
+
+
+def _id_expander(match):
+    org = model.Group.get(match.group(2))
+    disable_sub_search = toolkit.asbool(
+        org.extras.get('disable_hierarchy_search')
+    )
+
+    if disable_sub_search:
+        return match.group(0)
+    fq = ' OR '.join(
+        '"{}"'.format(org.id)
+        for org in org.get_children_groups('organization') + [org]
+    )
+
+    return 'owner_org:({})'.format(fq)
 
 
 class Hierarchy_SearchPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IPackageController, inherit=True)
+
+    # IPackageController
+
+    def before_search(self, data_dict):
+        if (g.controller, g.action) == ('organization', 'read'):
+
+            data_dict['fq'] = RE_ORG_FQ.sub(
+                _id_expander,
+                data_dict.get('fq', ''),
+            )
+
+        return data_dict
 
     # IConfigurer
 
